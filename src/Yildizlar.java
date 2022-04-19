f885882223000cac21b1e71f72455231e75710b3
orhanacikgoz <orhanacikgozz@outlook.com>
2022-04-19 16:36:50 +0300

    Star
    Yıldız çizimi

---
+++ src/Yildizlar.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Yildizlar {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Bir Sayi Giriniz:  ");
+        int n=input.nextInt();
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i=(n-1);i>=0;i--){
+            for (int j=0;j<(n-i);j++){
+                System.out.print(" ");
+            }
+            for (int k=0;k<(2 * i - 1); k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+
+
+
+
+
+
+    }}
+
+
+
+
